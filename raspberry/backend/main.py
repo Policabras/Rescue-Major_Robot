7c58c0e9a1295c6c961f73e84a3d7cf48e875c61
nana <2530022@upv.edu.mx>
2026-06-28 08:38:21 -0600

    Prueba de comunicación bidireccional básica con rasp y web

--- raspberry/backend/main.py
+++
@@ -1,7 +0,0 @@
-# Al inicio de tu main.py importas tu nuevo módulo
-from control_carro import iniciar_control
-
-# En la parte de tu código donde arranques tus procesos principales:
-if __name__ == "__main__":
-    # Esto activará el puente con el Arduino y el control
-    iniciar_control()
